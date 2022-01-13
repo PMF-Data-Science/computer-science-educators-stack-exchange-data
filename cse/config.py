6eb2437a15028343e5460fc1a9f0648c8047c278
Luka Farkaš <luka.farkas@gmail.com>
2022-01-13 17:11:40 +0100

    Revert "Delete cse directory"
    This reverts commit cc7baaeb9c99a0080a04e0b409e15c146b0736a6.

---
+++ cse/config.py
@@ -0,0 +1,5 @@
+### config.py ###
+import psycopg2
+# Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
+
+DATABASE_URL = 'postgresql+psycopg2://postgres:post016gre@localhost:5432/CSE'
