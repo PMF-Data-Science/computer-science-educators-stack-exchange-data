7b0e7a5b8f3fa38d183ab25b5d8d8dec4ef917a4
Luka Farkaš <luka.farkas@gmail.com>
2022-01-14 18:21:59 +0100

    Update config.py

--- cse/config.py
+++ cse/config.py
@@ -2,4 +2,4 @@
 import psycopg2
 # Scheme: "postgresql+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 
-DATABASE_URL = 'postgresql+psycopg2://postgres:post016gre@localhost:5432/CSE'
+DATABASE_URL = 'postgresql+psycopg2://postgres:post016gre@localhost:5432/cse'
