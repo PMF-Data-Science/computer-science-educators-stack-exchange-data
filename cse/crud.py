43e371c920e6639106925c77afca44b5297a958a
Luka Farkaš <luka.farkas@gmail.com>
2022-01-18 08:32:16 +0100

    Update crud.py
    connection bug removed

--- cse/crud.py
+++ cse/crud.py
@@ -3,8 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from config import DATABASE_URL
-
-Base = declarative_base()
+from models import Base
 
 engine = create_engine(DATABASE_URL)
 
